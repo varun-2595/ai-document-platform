1b7968c28661af61ca7ce42b8944a71a48150c09
Varun <varun@Varuns-MacBook-Air.local>
2026-05-18 02:47:35 +0530

    Add env configs

---
+++ backend/app/core/config.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Settings:
+    APP_NAME = os.getenv("APP_NAME")
+    APP_VERSION = os.getenv("APP_VERSION")
+    DEBUG = os.getenv("DEBUG")
+
+settings = Settings()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.api.health import router as health_router
+from app.core.config import settings
 
-app = FastAPI(title="AI Document Platform")
+app = FastAPI(title=settings.APP_NAME, version=settings.APP_VERSION)
 
 app.include_router(health_router)
